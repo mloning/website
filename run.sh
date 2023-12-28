365dce827bfe2cb7642ede44a0b42cbddbdc54a1
mloning <21020482+mloning@users.noreply.github.com>
2023-12-28 12:46:20 +0100

    update footer, minor formatting fixes

--- run.sh
+++ run.sh
@@ -2,13 +2,12 @@
 # Simple alternative to Makefile, inspired by https://death.andgravity.com/run-sh
 #
 # usage: ./run.sh <command> [<argument> ...]
-
 set -euo pipefail
 IFS=$'\n\t'
 
 # Run server for local development
 server () { 
-	hugo server --verbose --buildDrafts --buildFuture --navigateToChanged
+  hugo server --verbose --buildDrafts --buildFuture --navigateToChanged
 }
 
 # Add new post
